4706b10ddcb658b68372dd9e37c99b9b94b7427b
Boukrou_yasmine <159439423+lindayyy2334@users.noreply.github.com>
2026-02-24 12:00:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ st.header("💼 Choose Your Plan")
 col1, col2, col3 = st.columns(3)
 
 # Styles for cards and buttons
-card_style = "padding:20px; border-radius:15px; box-shadow:2px 2px 15px rgba(0,0,0,0.1); background-color:white; margin-bottom:20px;"
+card_style = "padding:20px; border-radius:15px; box-shadow:2px 2px 15px rgba(0,0,0,0.1); background-color:pink ; margin-bottom:20px;"
 button_color = "#FF6B81"
 
 # Free Plan
